5b67e09947a6795b67d1a95e9cf6828f0c418b6c
James Hester <jhester2@umd.edu>
2023-03-28 13:58:51 -0400

    Added a new route and form that the user can see, still working on modifying the contact page to push back an end message to the user.

--- app.py
+++ app.py
@@ -21,6 +21,17 @@ def favorite():
     print('Favorite Professor Entered: ' + request.args.get('favorite_professor'))
     return render_template('favorite-course.html')
 
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        print('First Name entered: ' + request.form.get('first_name'))
+        print('Last Name entered: ' + request.form.get('last_name'))
+        print('Email entered: ' + request.form.get('email'))
+
+    # Must check the checkbox to see if the website rocks
+    if request.form.get('agree_check'):
+        print('Agree this website rocks entered: ' + request.form.get('agree_check'))
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
