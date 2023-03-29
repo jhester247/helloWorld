35b0774219749ca1222979712a88221d5804caf9
James Hester <jhester2@umd.edu>
2023-03-29 14:29:50 -0400

    Ended up using a lot of bootstrap and youtube videos, had an extremly rough time throughout and am definitley going to have to put more work into this. It is very rough but i am getting user returns on the contact page but not on the favorite course page. I get a return through the software but I am struggling really hard. I am however happy with my contact page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -15,22 +15,32 @@ def hello():
 def about():
     return render_template('about.html')
 
-@app.route('/favorite-course') # added a new template
+@app.route('/favorite-course')
 def favorite():
-    print('Favorite Course Entered: ' + request.args.get('favorite_course'))
-    print('Favorite Professor Entered: ' + request.args.get('favorite_professor'))
-    return render_template('favorite-course.html')
+    favorite_course = request.args.get('favorite_course')
+    favorite_professor = request.args.get('favorite_professor')
+    print('Favorite Course Entered: ' + favorite_course)
+    print('Favorite Professor Entered: ' + favorite_professor)
+    return render_template('favorite-course.html', favorite_course=favorite_course, favorite_professor=favorite_professor)
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
-        print('First Name entered: ' + request.form.get('first_name'))
-        print('Last Name entered: ' + request.form.get('last_name'))
-        print('Email entered: ' + request.form.get('email'))
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        email = request.form.get('email')
+        agree_check = request.form.get('agree_check')
 
-    # Must check the checkbox to see if the website rocks
-    if request.form.get('agree_check'):
-        print('Agree this website rocks entered: ' + request.form.get('agree_check'))
+        if not first_name or not last_name or not email or not agree_check:
+            return render_template('contact.html', error_message='Please fill out all required fields')
+
+        print('First Name entered: ' + first_name)
+        print('Last Name entered: ' + last_name)
+        print('Email entered: ' + email)
+        print('Agree this website rocks entered: ' + agree_check)
+
+        return render_template('contact.html', form_submitted=True, first_name=first_name, last_name=last_name, email=email)
+
     return render_template('contact.html')
 
 
